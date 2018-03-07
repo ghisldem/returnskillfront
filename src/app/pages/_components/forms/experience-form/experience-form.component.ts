8ed8eb55614898e87cad202dcc3dc78454fe74b5
Farid <skylixhammer@gmail.com>
2018-03-07 14:39:17 +0100

    experience form

---
+++ src/app/pages/_components/forms/experience-form/experience-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl,FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-experience-form',
+  templateUrl: './experience-form.component.html',
+  styleUrls: ['./experience-form.component.css']
+})
+export class ExperienceFormComponent implements OnInit {
+  entreprise = new FormControl('', [Validators.required, Validators.email]);
+  options: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      'color': 'primary'
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  getErrorMessage() {
+    return this.entreprise.hasError('required') ? 'Vous devez renseigner ce champs' :
+        this.entreprise.hasError('entreprise') ? 'Entrez le nom de votre entreprise' :
+            '';
+  }
+
+}
