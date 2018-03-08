0d2c5967d184e956a99ec1fce62945120b8d9ae8
ghisldem <ghislain.demester@gmail.com>
2018-03-08 17:02:31 +0100

    menu bar begin code

--- src/app/pages/pages-view/administrator/administrator.module.ts
+++ src/app/pages/pages-view/administrator/administrator.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AdministratorRoutingModule, routedComponents } from './administrator-routing.module';
+import { ThemesModule } from '../../../themes/themes.module';
 
 @NgModule({
   imports: [
-    AdministratorRoutingModule
+    AdministratorRoutingModule,
+    ThemesModule
   ],
   declarations: [
     ...routedComponents,
