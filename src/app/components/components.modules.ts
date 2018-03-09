824670825b0e0d2209a9c6bbf29d96b66c94d789
ghisldem <ghislain.demester@gmail.com>
2018-03-09 17:04:18 +0100

    test modif architecture

---
+++ src/app/components/components.modules.ts
@@ -0,0 +1,63 @@
+/**
+ * Modules
+ */
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+import { ThemesModule } from '../themes/themes.module';
+import { ServicesModule } from '../services/services.module';
+
+import { MaterialModule } from './material.module';
+
+/**
+ * Modules material/animation/font
+ */
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+
+/**
+ * services
+ */
+import { UserService } from '../services/data/user.service';
+
+/**
+ * components
+ */
+
+import {GenericTableComponent} from './generic-table/generic-table.component';
+
+@NgModule({
+    imports: [
+        /**
+         * modules @angular
+         */
+        ServicesModule,
+        FormsModule,
+        CommonModule,
+        ReactiveFormsModule,
+        ThemesModule,
+        MaterialModule,
+        AngularFontAwesomeModule,
+
+    ],
+
+    declarations: [
+
+        /**
+         * components
+         */
+        GenericTableComponent,
+    ],
+    providers: [
+        UserService
+    ],
+
+    exports: [
+
+    ]
+})
+export class PagesModule { }
---
+++ src/app/components/generic-table/generic-table.component.ts
@@ -0,0 +1,133 @@
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+
+import { MatPaginator, MatSort, MatTableDataSource, MatInputModule } from '@angular/material';
+
+import {Observable} from 'rxjs/Observable'
+import {merge} from 'rxjs/observable/merge';
+import {of as observableOf} from 'rxjs/observable/of';
+import {catchError} from 'rxjs/operators/catchError';
+import {map} from 'rxjs/operators/map';
+import {startWith} from 'rxjs/operators/startWith';
+import {switchMap} from 'rxjs/operators/switchMap';
+
+
+@Component({
+  selector: 'app-generic-table-test',
+  templateUrl: './generic-table.component.html',
+  styleUrls: ['./generic-table.component.css']
+})
+
+export class GenericTableComponent implements OnInit {
+
+  @Input() observableDataTable : Observable<any[]>
+  @Input() columnsData: Array<[string, string]>;
+  @Input() actions : boolean = false;
+
+  
+  searchBarVisible :  boolean =  false;
+  dataTable: any[];
+  displayColumns: Column[];
+  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
+  displayedColumnsAndActions : String [];
+  dataSource = new MatTableDataSource();
+
+  
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+    this.updateDisplayedColumns();
+    this.updateColumns();
+    this.observableDataTable.subscribe(data => {
+ 
+      this.dataSource.data = data;
+      this.ngAfterViewInit;
+    });
+
+  }
+
+  /** 
+   * update data columns model
+  */
+  updateDisplayedColumns() {
+    const tabProperty: string[] = [];
+    const tabProperty2: string[] = [];
+    for (const infoColumn of this.columnsData) {
+      tabProperty.push(infoColumn[0]);
+      tabProperty2.push(infoColumn[0]);
+    }
+
+    if (this.actions){ 
+      this.displayedColumnsAndActions = tabProperty2;
+      this.displayedColumnsAndActions.push('actionsColumn');
+    } else{
+      this.displayedColumns = tabProperty;
+      this.displayedColumnsAndActions =this.displayedColumns;
+        }
+
+  }
+
+  updateColumns(){
+    const tabColumns: Column[] = [];
+    for (const infoColumn of this.columnsData) {
+      const col : Column = new Column();
+      col.nameProperty = infoColumn[0];
+      col.nameTitle = infoColumn[1];
+      tabColumns.push(col);
+    }
+    this.displayColumns = tabColumns;
+  }
+
+  /**
+   * Set the paginator and sort after the view init since this component will
+   * be able to query its view for the initialized paginator and sort.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  /**
+   * function display search bar
+   */
+
+   onClickDisplaySearchBar(){
+    if (this.searchBarVisible){
+this.searchBarVisible = false;
+    }else{
+      this.searchBarVisible = true;
+          }
+
+   }
+
+
+}
+
+
+
+
+class Column {
+  nameProperty: string;
+  nameTitle: String;
+}
+
+
+
+
+
+
+
+
