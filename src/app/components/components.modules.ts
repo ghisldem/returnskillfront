6a4dcfd2f070b82c587814eac0c7ec5d1e0b1a18
Unknown <ghislain.demester@gmail.com>
2018-03-11 07:39:20 +0100

    transfert generic table in module components

--- src/app/components/components.modules.ts
+++ src/app/components/components.modules.ts
@@ -26,18 +26,18 @@ import { UserService } from '../services/data/user.service';
 /**
  * components
  */
-import {ExperienceFormComponent} from './forms/experience-form/experience-form.component';
-import {GenericAutocompleteComponent} from './forms/generic-autocomplete/generic-autocomplete.component';
-import {LoginFormComponent} from './forms/login-form/login-form.component';
-import {RegisterFormComponent} from './forms/register-form/register-form.component';
-import {SkillFormComponent} from './forms/skill-form/skill-form.component';
-import {TrainingFormComponent} from './forms/training-form/training-form.component';
-import {UserCompleteFormComponent} from './forms/user-complete-form/user-complete-form.component';
-import {} from './forms';
-import {} from './forms';
-import {} from './forms';
-import {} from './forms';
-import {GenericTableComponent} from './generic-table/generic-table.component';
+import { ExperienceFormComponent } from './forms/experience-form/experience-form.component';
+import { GenericAutocompleteComponent } from './forms/generic-autocomplete/generic-autocomplete.component';
+import { LoginFormComponent } from './forms/login-form/login-form.component';
+import { RegisterFormComponent } from './forms/register-form/register-form.component';
+import { SkillFormComponent } from './forms/skill-form/skill-form.component';
+import { TrainingFormComponent } from './forms/training-form/training-form.component';
+import { UserCompleteFormComponent } from './forms/user-complete-form/user-complete-form.component';
+import { } from './forms';
+import { } from './forms';
+import { } from './forms';
+import { } from './forms';
+import { GenericTableComponent } from './tables/generic-table/generic-table.component';
 
 @NgModule({
     imports: [
@@ -56,7 +56,7 @@ import {GenericTableComponent} from './generic-table/generic-table.component';
     declarations: [
 
         /**
-         * components
+         * components - forms
          */
         ExperienceFormComponent,
         GenericAutocompleteComponent,
@@ -65,7 +65,12 @@ import {GenericTableComponent} from './generic-table/generic-table.component';
         SkillFormComponent,
         TrainingFormComponent,
         UserCompleteFormComponent,
+
+        /**
+         * components - tables
+         */
         GenericTableComponent,
+        
     ],
     providers: [
         UserService
@@ -73,12 +78,12 @@ import {GenericTableComponent} from './generic-table/generic-table.component';
 
     exports: [
         ExperienceFormComponent,
-        GenericTableComponent,
         LoginFormComponent,
         RegisterFormComponent,
         SkillFormComponent,
         TrainingFormComponent,
         UserCompleteFormComponent,
+        GenericTableComponent,
         MaterialModule,
     ]
 })
